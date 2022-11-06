2a6fadb50064994e5f4bdb2b21810fff378126c0
himitery <himit0131@gmail.com>
2022-11-06 19:21:09 +0900

    fix(package): product attachment 파일 업로등 오류 수정

--- src/main/java/com/github/org/todaybread/todaybread/product/attachment/application/ProductAttachmentServiceImpl.java
+++ src/main/java/com/github/org/todaybread/todaybread/product/attachment/application/ProductAttachmentServiceImpl.java
@@ -1,9 +1,9 @@
 package com.github.org.todaybread.todaybread.product.attachment.application;
 
+import com.github.org.todaybread.todaybread.file.application.facade.FileFacade;
 import com.github.org.todaybread.todaybread.file.application.service.file.FileService;
 import com.github.org.todaybread.todaybread.file.domain.File;
 import com.github.org.todaybread.todaybread.file.domain.FileType;
-import com.github.org.todaybread.todaybread.member.application.service.MemberService;
 import com.github.org.todaybread.todaybread.product.attachment.domain.ProductAttachment;
 import com.github.org.todaybread.todaybread.product.attachment.domain.ProductAttachmentRepository;
 import com.github.org.todaybread.todaybread.product.domain.Product;
@@ -18,8 +18,8 @@ import org.springframework.web.multipart.MultipartFile;
 public class ProductAttachmentServiceImpl implements ProductAttachmentService {
 
     private final ProductAttachmentRepository productAttachmentRepository;
+    private final FileFacade fileFacade;
     private final FileService fileService;
-    private final MemberService memberService;
 
     @Override
     public ProductAttachment getByFileId(String fileId) {
@@ -29,7 +29,7 @@ public class ProductAttachmentServiceImpl implements ProductAttachmentService {
     @Override
     @Transactional
     public ProductAttachment save(String memberId, Product product, MultipartFile upload) {
-        File file = fileService.save(memberService.getMember(memberId), FileType.PRODUCT, upload);
+        File file = fileFacade.upload(memberId, FileType.PRODUCT, upload);
 
         return productAttachmentRepository.save(
             ProductAttachment.builder()
