e23dc6180e3ac27f0e9ff326d1b51715c1ff12da
Bae-jihyun <govl6113@gmail.com>
2022-11-12 01:00:04 +0900

    fix(auth): logout redis 문제 해결

--- src/main/java/com/github/org/todaybread/todaybread/file/application/service/file/FileServiceImpl.java
+++ src/main/java/com/github/org/todaybread/todaybread/file/application/service/file/FileServiceImpl.java
@@ -21,7 +21,8 @@ public class FileServiceImpl implements FileService {
     private static final List<String> ALLOWED_FILE_TYPE = Arrays.asList(
         "jpeg",
         "png",
-        "jpg"
+        "jpg",
+        "heic"
     );
     private final FileRepositoryImpl fileRepository;
     private final StorageServiceImpl storageService;
