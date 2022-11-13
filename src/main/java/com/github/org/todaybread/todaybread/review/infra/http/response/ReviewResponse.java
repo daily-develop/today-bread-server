7a36d018664a9f5e27acc6c41cba817a90d483a9
himitery <himit0131@gmail.com>
2022-11-14 03:21:00 +0900

    fix(review): review response 수정

--- src/main/java/com/github/org/todaybread/todaybread/review/infra/http/response/ReviewResponse.java
+++ src/main/java/com/github/org/todaybread/todaybread/review/infra/http/response/ReviewResponse.java
@@ -1,9 +1,12 @@
 package com.github.org.todaybread.todaybread.review.infra.http.response;
 
 import com.github.org.todaybread.todaybread.file.infra.http.response.FileResponse;
+import com.github.org.todaybread.todaybread.member.infra.http.response.MemberResponse;
+import com.github.org.todaybread.todaybread.product.infra.http.response.ProductResponse;
 import com.github.org.todaybread.todaybread.review.domain.Review;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,23 +14,25 @@ import lombok.Getter;
 public class ReviewResponse {
 
     String id;
-
     LocalDateTime createdAt;
-
     LocalDateTime updatedAt;
-
-    List<FileResponse> attachment;
+    MemberResponse member;
+    ProductResponse product;
     Float score;
-
     String content;
+    List<FileResponse> attachment;
 
     @Builder
     public ReviewResponse(Review review) {
         this.id = review.getId().toString();
         this.createdAt = review.getCreatedAt();
         this.updatedAt = review.getUpdatedAt();
-        this.attachment = List.of();
+        this.member = review.getMember().toResponse();
+        this.product = review.getProduct().toResponse();
         this.score = review.getScore();
         this.content = review.getContent();
+        this.attachment = review.getAttachments().stream()
+            .map(it -> it.getFile().toResponse())
+            .collect(Collectors.toList());
     }
 }
